abe2ce9fe290359fc5b9cbab948c9d5fe19beb50
ericka lus <lusericka0@gmail.com>
2023-02-05 07:57:26 +0100

    first commit

---
+++ src/Pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./home.css"
+import Navbar from '../../Components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'><Navbar/></div>
+  )
+}
+
+export default Home
