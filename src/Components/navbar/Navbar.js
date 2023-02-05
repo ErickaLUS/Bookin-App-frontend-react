64fa8efd0de4d38de414e39ac8d24a1f123925ef
ericka lus <lusericka0@gmail.com>
2023-02-05 08:17:32 +0100

    added navbar

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Mollbooking</span>
+        <div className="navItems">
+          <button className="navButton">Registrer</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
