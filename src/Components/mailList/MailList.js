75ad1ddd433863651b126cf218a73d697559f96c
ericka lus <lusericka0@gmail.com>
2023-02-06 14:38:44 +0100

    add mailList

---
+++ src/Components/mailList/MailList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './mailList.css'
+
+const MailList = () => {
+  return (
+    <div className='mail'>
+      
+    <h1 className='mailTitle'>Save Time, Save money!</h1>
+    <span className='mailDesc'>sign up and we'll send the best deals to you</span>
+    <div className='mailInputContainer'>
+      <input type="text" placeholder='your mail'/>
+      <button>subscribe</button>
+    </div>
+    </div>
+  )
+}
+
+export default MailList
