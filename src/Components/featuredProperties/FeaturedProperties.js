b6da298a8a6e1fcc5edca9c92bbbc5e283b8b52d
ericka lus <lusericka0@gmail.com>
2023-02-06 12:38:52 +0100

    added featured proporties

---
+++ src/Components/featuredProperties/FeaturedProperties.js
@@ -0,0 +1,69 @@
+import React from "react";
+import './featuredProperties.css'
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fProperties">
+      <div className="fPropertiesItem">
+        <img
+          src="https://images.pexels.com/photos/13164550/pexels-photo-13164550.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotels Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+
+      <div className="fPropertiesItem">
+        <img
+          src="https://images.pexels.com/photos/13164550/pexels-photo-13164550.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotels Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+
+      <div className="fPropertiesItem">
+        <img
+          src="https://images.pexels.com/photos/13164550/pexels-photo-13164550.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotels Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fPropertiesItem">
+        <img
+          src="https://images.pexels.com/photos/13164550/pexels-photo-13164550.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotels Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
